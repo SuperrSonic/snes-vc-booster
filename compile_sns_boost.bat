ce8220e464215cbd61579c9fe812dd64a6397478
Diego A. <Diego_Arroyo@live.com>
2024-12-21 03:29:50 -0400

    Add hb nandloaders and src

---
+++ compile_sns_boost.bat
@@ -0,0 +1,2 @@
+"C:\devkitPro\msys2\MinGW\bin\gcc" "D:\Documents\Projects\SNES_VC_BOOST\sns_boost.c" -o "D:\Documents\Projects\SNES_VC_BOOST\sns_boost.exe" icon.res
+PAUSE
---
+++ run_sns_boost.bat
@@ -0,0 +1,2 @@
+sns_boost.exe -i 01.app --volume --no-dark --no-cc --gc-remap --wiimote-native --no-header-check-simple --verbose
+PAUSE
